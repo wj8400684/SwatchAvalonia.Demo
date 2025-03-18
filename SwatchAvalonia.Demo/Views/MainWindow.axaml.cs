4b15869aaa297b2c386e133b60349a5f2c877ba4
吴俊 <8400684@qq.com>
2025-03-18 15:04:04 +0800

    55

--- SwatchAvalonia.Demo/Views/MainWindow.axaml.cs
+++ SwatchAvalonia.Demo/Views/MainWindow.axaml.cs
@@ -5,19 +5,36 @@ using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Controls.Notifications;
 using Avalonia.Input;
+using Avalonia.Media.Immutable;
+using Avalonia.Media;
+using Avalonia.VisualTree;
 using Ursa.Controls;
 
 namespace SwatchAvalonia.Demo.Views
 {
     public partial class MainWindow : UrsaWindow
     {
-      
-        
+
+
         public MainWindow()
         {
             InitializeComponent();
+            IDisposable? topLevelBackgroundSideSetter = null, sideBarBackgroundSetter = null, paneBackgroundSetter = null;
+
+            topLevelBackgroundSideSetter?.Dispose();
+            sideBarBackgroundSetter?.Dispose();
+            paneBackgroundSetter?.Dispose();
+
+            var topLevel = (TopLevel)this.GetVisualRoot()!;
+            topLevel.TransparencyLevelHint = new[] { WindowTransparencyLevel.Mica };
+
+            var transparentBrush = new ImmutableSolidColorBrush(Colors.White, 0);
+            var semiTransparentBrush = new ImmutableSolidColorBrush(Colors.Gray, 0.2);
+            topLevelBackgroundSideSetter = topLevel.SetValue(BackgroundProperty, transparentBrush, Avalonia.Data.BindingPriority.Style);
+            //sideBarBackgroundSetter = sideBar.SetValue(BackgroundProperty, semiTransparentBrush, Avalonia.Data.BindingPriority.Style);
+            //paneBackgroundSetter = sideBar.SetValue(SplitView.PaneBackgroundProperty, semiTransparentBrush, Avalonia.Data.BindingPriority.Style);
         }
-        
-     
+
+
     }
 }
